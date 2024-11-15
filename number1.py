dcc6fe06898e3490bcf111dd751d75192f5564cc
Farhin Rahman <farhin.rahmanp@gmail.com>
2024-11-15 13:23:56 +0600

    updated explanation of task-1

--- number1.py
+++ number1.py
@@ -14,13 +14,13 @@ def convert_coordinate(x,y):
     b = (W_Height/2) - y 
     return a,b
 
-def draw_points(x, y, s):
+def draw_points(x, y, s): #used for the door knob
     glPointSize(s)
     glBegin(GL_POINTS)
     glVertex2f(x,y)
     glEnd()
 
-def iterate():
+def iterate(): #used for the viewport of the 2D drawing 
     glViewport(0, 0, 500, 500)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
@@ -38,7 +38,7 @@ def drawAxes(): #1(i)
     glVertex2d(173,0)
     glEnd()
 
-    #white roof using triangles
+    #white inner roof using triangles
     glColor3f(1,1,1)
     glBegin(GL_TRIANGLES)
     glVertex2d(-150,0)
@@ -108,47 +108,48 @@ def drawAxes(): #1(i)
 
     glEnd()
 
-def generateRaindrops():
-    global raindrops, flag, mode, rain_direction
-    glColor3f(0,0,0)
-    if mode=="night":
+def generateRaindrops(): #it draws the raindrops
+    global raindrops, flag, mode, rain_direction #globally declared so that we can use them outside the function
+    glColor3f(0,0,0) #black color for raindrops
+    if mode=="night": #if night mode is on, then white color for raindrops
         glColor3f(1,1,1)
-    else:
+    else: #else black color for raindrops
         glColor3f(0,0,0)
 
-    glLineWidth(3)
+    glLineWidth(3) #line width of raindrops
     glBegin(GL_LINES)
-    for i in range(0,150):
-        if flag!=True:
-            x=random.randint(-250,250)
-            y_start=random.randint(-10,500)
-            y_end= y_start-random.randint(20,40)
-            rain_coordinate=[x,y_start,y_end]
-            raindrops.append(rain_coordinate)
+    for i in range(0,150): #generating 150 raindrops
+        if flag!=True: #if raindrops are not generated yet, then generate them
+            x=random.randint(-250,250) #random x coordinate
+            y_start=random.randint(-10,500) #random y coordinate from the top
+            y_end= y_start-random.randint(20,40) #random y coordinate for the bottom of the raindrop by subtracting a random value from y-start to give a length
+            rain_coordinate=[x,y_start,y_end] #storing the coordinates in a list, a list will represent 1 raindrop
+            raindrops.append(rain_coordinate) #appending the 150 raindrops to the list
 
-        else:
-            glVertex2f(raindrops[i][0], raindrops[i][1])
-            glVertex2f(raindrops[i][0]+rain_direction, raindrops[i][2])    
+        else: #this handles the direction of the raindrop
+            glVertex2f(raindrops[i][0], raindrops[i][1]) #x and y-start coordinate of the raindrop
+            glVertex2f(raindrops[i][0]+rain_direction, raindrops[i][2]) #x and y-end, here x is added with rain_direction to change the direction of the raindrop
+            #for example, if rain_direction is 1, then the raindrop will move to the right, if -1, then to the left. if x is 0, then it will go straight downwards
     glEnd()
 
-    flag=True
+    flag=True #bar bar same coordinates create korbena 
 
 
 
-def animate():
-    glutPostRedisplay()
-    global raindrops, rain_direction,mode
-    for rain_coordinate in raindrops:
-        if rain_coordinate[1] <= 0:
+def animate(): #updates the position of all the raindrops
+    glutPostRedisplay() #redraws, continous movement of the raindrops
+    global raindrops, rain_direction
+    for rain_coordinate in raindrops: #each rain coordinate is a list of 3 elements, x, y-start, y-end
+        if rain_coordinate[1] <= 0: #if y start is less than or equal to 0, then it means the raindrop has reached the bottom. So, we need to reset the raindrop
             rain_coordinate[1] += 500
             rain_coordinate[2] += 500
-        else:
+        else: #if the y-start is greater than 0 hasn't reached the bottom yet, then we need to move the raindrop downwards
             rain_coordinate[1] -= 1
             rain_coordinate[2] -= 1        
         
 
 
-def keyboardListener(key, x, y): #1(iii)
+def keyboardListener(key, x, y): #1(iii) for switching between night and morning mode
 
     global mode
     if key==b'1':
@@ -170,18 +171,6 @@ def specialListener(key,x,y):
         print("Rain direction set to right")
     glutPostRedisplay()
 
-
-def mouseListener(button, state, x, y):
-    print(x,y)
-    # global rain_direction
-    # if button == GLUT_LEFT_BUTTON and state == GLUT_DOWN:
-    #         rain_direction = -1
-    #         print("Rain direction set to left")
-    # elif button == GLUT_RIGHT_BUTTON and state == GLUT_DOWN:
-    #         rain_direction = 1
-    #         print("Rain direction set to right")
-    # glutPostRedisplay()
-
 def display():
     global mode
     if mode=="morning":
@@ -198,7 +187,6 @@ def display():
 
 
 def init():
-    glClearColor(0,0,0,0)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     gluPerspective(104,1,1,1000.0)
@@ -213,7 +201,6 @@ wind = glutCreateWindow(b"OpenGL Coding Practice")
 init()
 glutDisplayFunc(display)	#display callback function
 glutKeyboardFunc(keyboardListener)
-glutMouseFunc(mouseListener)
 glutSpecialFunc(specialListener)
 glutIdleFunc(animate)
 
